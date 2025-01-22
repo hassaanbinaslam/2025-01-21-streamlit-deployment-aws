97df02d3e3b1527ac2b1128639f93df9c590ad79
hassaanbinaslam <hassaanbinaslam@gmail.com>
2025-01-22 21:56:48 +0500

    streamlit fargate base code

--- streamlit_fargate/streamlit_fargate/streamlit_fargate_stack.py
+++ streamlit_fargate/streamlit_fargate/streamlit_fargate_stack.py
@@ -1,19 +1,45 @@
 from aws_cdk import (
-    # Duration,
+    aws_ecs as ecs,
+    aws_ec2 as ec2,
+    aws_ecs_patterns as ecs_patterns,
     Stack,
-    # aws_sqs as sqs,
+    CfnOutput,
 )
+
 from constructs import Construct
+
 
 class StreamlitFargateStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Create a VPC for the ECS Cluster
+        vpc = ec2.Vpc(
+            self, "StreamlitFargateVpc", max_azs=1  # Default is all AZs in the region
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "StreamlitFargateQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Create an ECS Cluster
+        cluster = ecs.Cluster(self, "StreamlitFargateCluster", vpc=vpc)
+
+        # Build Dockerfile from local folder and push to ECR
+        image = ecs.ContainerImage.from_asset("docker_app")
+
+        # Define a Fargate Task Definition and Service
+        fargate_service = ecs_patterns.ApplicationLoadBalancedFargateService(
+            self,
+            "StreamlitFargateService",
+            cluster=cluster,
+            task_image_options={
+                "image": image,
+                "container_port": 8501,  # Streamlit default port=8501. Should be adjusted based on Dockerfile
+            },
+            public_load_balancer=True,
+        )
+
+        # Output the Load Balancer URL
+        CfnOutput(
+            self,
+            "LoadBalancerDNS",
+            value=fargate_service.load_balancer.load_balancer_dns_name,
+        )
